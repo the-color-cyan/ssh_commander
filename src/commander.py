b03f81468702d087c72d5b8f5a51fb3840d07bb9
Brian Williamson <C-Brian.Williamson@charter.com>
2019-07-17 15:18:23 -0600

    Update commander.py

--- src/commander.py
+++ src/commander.py
@@ -1,6 +1,6 @@
 import sys
 import paramiko as pm
-import netmiko
+import netmiko as nm
 
 if len(sys.argv) < 4:
     print("missing arguments")
@@ -13,44 +13,61 @@ password = sys.argv[4]
 
 port = 22
 
-hostfile = '../data/hosts/' + hosts
+hostfile = '../data/hosts/' + hosts + '.json'
 commandfile = '../data/commands/' + commands
 logfile = '../data/log/commander.log'
 
-for file in [hostfile, commandfile]:
-    try:
-        with open(file, 'r') as f:
-            if file == hostfile:
-                hostlist = f.read().splitlines()
-            elif file == commandfile:
-                commandlist = f.read().splitlines()
-    except IOError:
-        print(file + ' not found.')
-        sys.exit(1)
-    except:
-        print('Error: ', sys.exc_info()[0], sys.exc_info()[1])
-        sys.exit(1)
+testhost = {
+    "host": "192.168.250.255"
+    "device_type": "juniper"
+}
 
-for host in hostlist:
-    try:
-        client = pm.SSHClient()
-        client.load_system_host_keys()
-        client.set_missing_host_key_policy(pm.AutoAddPolicy())
-        client.connect(host, port=port, username=username, password=password)
-        print('Connection succesful')
-        for command in commandlist:
-            try:
-                print('Executing:', command)
-                stdin, stdout, stderr = client.exec_command(command)
-                print('SSH:', stdout.read())
-                print('SSH:', stderr.read())
-            except:
-               print('Error: ', sys.exc_info()[0], sys.exc_info()[1])
-               client.close()
-    except:
-        print('Error: ', sys.exc_info()[0], sys.exc_info()[1])
-        client.close()
-    finally:
-        client.close()
+testconfigs = [
+    'set snmp trap-group ace-nagios-snmp-traps targets 192.168.255.3', 
+    'delete snmp trap-group ace-nagios-snmp-traps targets 192.168.255.12'
+]
+
+client = nm.Netmiko(**testhost)
+
+print()
+print(client.find_prompt())
+output = net_connect.send_config_set(testconfigs)
+
+
+#for file in [hostfile, commandfile]:
+#    try:
+#        with open(file, 'r') as f:
+#            if file == hostfile:
+#                hostlist = f.read().splitlines()
+#            elif file == commandfile:
+#                commandlist = f.read().splitlines()
+#    except IOError:
+#        print(file + ' not found.')
+#        sys.exit(1)
+#    except:
+#        print('Error: ', sys.exc_info()[0], sys.exc_info()[1])
+#        sys.exit(1)
+
+#for host in hostlist:
+#    try:
+#        client = pm.SSHClient()
+#        client.load_system_host_keys()
+#        client.set_missing_host_key_policy(pm.AutoAddPolicy())
+#        client.connect(host, port=port, username=username, password=password)
+#        print('Connection succesful')
+#        for command in commandlist:
+#            try:
+#                print('Executing:', command)
+#                stdin, stdout, stderr = client.exec_command(command)
+#                print('SSH:', stdout.read())
+#                print('SSH:', stderr.read())
+#            except:
+#               print('Error: ', sys.exc_info()[0], sys.exc_info()[1])
+#               client.close()
+#    except:
+#        print('Error: ', sys.exc_info()[0], sys.exc_info()[1])
+#        client.close()
+#    finally:
+#        client.close()
 
 
